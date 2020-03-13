acc7d0d6f1e3019131c9fcb45328834dbf7a1945
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2020-03-13 19:28:30 +0600

    fixed all

--- src/Componets/Header/Header.js
+++ src/Componets/Header/Header.js
@@ -5,7 +5,9 @@ import logo from '../../logo.svg';
 const Header = () => {
     return (
         <div className="header">
-            <img src={logo} alt=""/><h3>Salary Management App</h3>
+            <img src={logo} alt=""/>
+            <h3 style={{color: 'deeppink'}}>REACT USERS INFORMATION AND SALARY APP</h3>
+            <img src={logo} alt=""/>
         </div>
     );
 };
